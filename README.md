fd3efa1b5b3ca135829efd385225444b1076aab6
Sagnik-Dey <sagnikdey77@gmail.com>
2021-10-06 18:09:33 -0700

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Number Facts Web App
---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("#form");
+const outputDiv = document.querySelector("#output");
+const numberTxt = document.querySelector("#numberTxt")
+
+form.addEventListener("submit", generateNumberFact);
+
+function generateNumberFact(e)
+{
+    e.preventDefault();
+    let number = numberTxt.value;
+    let outputHtml = "";
+
+    if (number === "")
+    {
+        outputHtml += `<h3> Enter a number to generate the fact </h3>`;
+        outputDiv.innerHTML = outputHtml;
+    }
+    else
+    {
+        const url = `http://numbersapi.com/${number}`;
+
+        const factXhr = new XMLHttpRequest();
+        factXhr.open("GET", url, true);
+
+        factXhr.onload = function ()
+        {
+            outputHtml += `<h3> ${this.responseText} </h3>`
+            outputDiv.innerHTML = outputHtml;
+        }
+
+        factXhr.send();
+    }
+}
